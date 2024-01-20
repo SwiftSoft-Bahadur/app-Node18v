736d2d16b46b822678c83933791635f66a3f27c5
Bahadur <bk209203@gmail.com>
2024-01-21 00:36:42 +0530

    package.json script updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     // Wildcard route for 404 page
+  { path: '**', component: NotFoundComponent },
+];
