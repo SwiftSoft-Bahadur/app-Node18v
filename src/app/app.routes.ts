23f98d885315b89b3c874b1095a99007b2bef533
Bahadur <bk209203@gmail.com>
2024-01-21 01:19:49 +0530

    package.json script updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { AuthComponent } from './auth/auth.component';
 
 export const routes: Routes = [
+     // Wildcard route for 404 page
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'auth', component: AuthComponent },
 ];
