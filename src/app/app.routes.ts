b70756c1edbef95b164dce6e041e0c152582f7c2
Bahadur Khan <70307617+SwiftSoft-Bahadur@users.noreply.github.com>
2024-02-05 15:46:38 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { UsersComponent } from './users/users.component';
 import { PostsComponent } from './posts/posts.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/app', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'posts', component: PostsComponent },
 ];
